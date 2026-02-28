07e659a7e69087ce37f0196c9f98f20b468395be
Mohammad Shariatmadari <mohammad.shariatmadari@oracle.com>
2026-02-28 07:26:09 -0800

    test file was added

--- nearest.py
+++ nearest.py
@@ -3,7 +3,7 @@ def nearest_square(num):
     Returns the nearest perfect square to the given number.
     """
     root = 0
-    while (root + 1) ** 2 <= num:
+    while (root +1) ** 2 < num:
         root += 1
     return root ** 2
 
