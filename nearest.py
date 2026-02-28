cb360ba3a8d00f658b9ddbb4b1890e15a04a0676
Mohammad Shariatmadari <mohammad.shariatmadari@oracle.com>
2026-02-28 06:51:27 -0800

    added nearest.py file

---
+++ nearest.py
@@ -0,0 +1,9 @@
+def nearest_square(num):
+    """
+    Returns the nearest perfect square to the given number.
+    """
+    root = 0
+    while (root + 1) ** 2 <= num:
+        root += 1
+    return root ** 2
+
